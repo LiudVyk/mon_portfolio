293fd7ed31718f7ba9e35760ffdf61aef8fd62d5
Liudmyla Mudra <lyudmula.mydra@gmail.com>
2023-01-27 12:00:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+//Commencer par l'appel du controller
+//require(controllers/...)
+
+define("PAGE_TITLE", "Accueil");
+?>
+
+<?php include("./assets/inc/front/head.php"); ?>
+<?php include("./assets/inc/front/header.php");?>
+<main>
+<h1> Coucou</h1>
+</main>
+
+<?php include("./assets/inc/front/footer.php");?>
